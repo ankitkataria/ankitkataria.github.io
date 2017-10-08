9218d80594f61414f714c60b966777264623406d
ankitkataria <ankitkataria28@gmail.com>
2017-10-08 12:04:28 +0530

    feat(Resume): added resume and refactored code

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const leftPane= document.getElementsByClassName('left-pane')[0];
+  	const container = document.getElementsByClassName('container')[0];
+  	const navList = document.getElementsByClassName('nav-list')[0];
+
+  	function navOpen(){
+  		leftPane.classList.toggle('open');
+  		container.classList.toggle('open');
+  		navList.classList.toggle('open');
+  	}
