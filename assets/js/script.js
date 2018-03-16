03715d668277b063f054dd95b6dd998b274664c8
ankitkataria <ankitkataria28@gmail.com>
2018-03-16 09:52:29 +0530

    Changes directory structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const leftPane= document.getElementsByClassName('left-pane')[0];
-  	const container = document.getElementsByClassName('container')[0];
-  	const navList = document.getElementsByClassName('nav-list')[0];
+const container = document.getElementsByClassName('container')[0];
+const navList = document.getElementsByClassName('nav-list')[0];
 
   	function navOpen(){
   		leftPane.classList.toggle('open');
