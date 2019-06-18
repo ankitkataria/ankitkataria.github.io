70aa05d88c1f3707f3a2a2ef7d65dc4b1c8a7230
Ankit Kataria <ankitkataria28@gmail.com>
2019-06-18 12:41:32 +0530

    Make blog the index page, also move image to left nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const leftPane= document.getElementsByClassName('left-pane')[0];
 const container = document.getElementsByClassName('container')[0];
 const navList = document.getElementsByClassName('nav-list')[0];
+const navListBtn = document.getElementsByClassName('nav-btn')[0];
 
   	function navOpen(){
   		leftPane.classList.toggle('open');
   		container.classList.toggle('open');
   		navList.classList.toggle('open');
+  		navListBtn.classList.toggle('nav-btn--open');
   	}
